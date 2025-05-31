8f7e8857b9d915bbc862167450897c8e4f2c4da2
Alexander Wodarz <kontakt@alexanderwodarz.de>
2025-05-31 14:28:46 +0200

    Add json array as body

--- src/main/java/de/alexanderwodarz/code/rest/ClientThread.java
+++ src/main/java/de/alexanderwodarz/code/rest/ClientThread.java
@@ -7,13 +7,19 @@ import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 import lombok.Getter;
 import lombok.Setter;
-import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +33,7 @@ public class ClientThread extends Thread {
     private RequestMethod method;
     private String response;
     private JSONObject body;
+    private JSONArray bodyArray;
     private Response clientResponse;
     private HashMap<String, String> headers = new HashMap<>();
     private Map<String, NewCookie> cookies = new HashMap<>(); // Stores cookies
@@ -45,9 +52,41 @@ public class ClientThread extends Thread {
         ClientConfig config = new ClientConfig();
         config.register(MultiPartFeature.class);
         config.register(new org.glassfish.jersey.jackson.JacksonFeature());
-        config.connectorProvider(new ApacheConnectorProvider());  // Use Apache connector
-        this.client = ClientBuilder.newClient(config);
+
+        this.client = ClientBuilder.newBuilder()
+                .sslContext(createTrustAllSSLContext())
+                .hostnameVerifier((s, sslSession) -> true)
+                .withConfig(config)
+                .build();
+
         this.webTarget = client.target(url);
+    }
+
+    public static SSLContext createTrustAllSSLContext() {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            TrustManager[] trustAllCertificates = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            };
+
+            sslContext.init(null, trustAllCertificates, new SecureRandom());
+            return sslContext;
+        } catch (Exception e) {
+            throw new RuntimeException("Fehler beim Erstellen des SSL-Kontexts", e);
+        }
     }
 
     public void addCookie(String name, String value) {
@@ -58,7 +97,7 @@ public class ClientThread extends Thread {
     public void run() {
         invocationBuilder = webTarget.request(type);
         headers.forEach((key, value) -> invocationBuilder.header(key, value));
-        cookies.values().forEach(cookie -> invocationBuilder.cookie(cookie));  // Add cookies to the request
+        cookies.values().forEach(cookie -> invocationBuilder.cookie(cookie));
 
         switch (method) {
             case GET:
@@ -95,7 +134,7 @@ public class ClientThread extends Thread {
         } else if (isRaw) {
             return Entity.text(rawBody);
         } else {
-            return Entity.json(body != null ? body.toString() : "{}");
+            return Entity.json(body != null ? body.toString() : bodyArray != null ? bodyArray.toString() :  "{}");
         }
     }
 
